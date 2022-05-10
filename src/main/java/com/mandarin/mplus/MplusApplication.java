a9c9839b07e22c17c826373bdbe4f61fc6bc752e
seungyeonpark <siaayeon@gmail.com>
2022-05-10 15:04:32 +0900

    Initial commit

---
+++ src/main/java/com/mandarin/mplus/MplusApplication.java
@@ -0,0 +1,13 @@
+package com.mandarin.mplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MplusApplication.class, args);
+	}
+
+}
