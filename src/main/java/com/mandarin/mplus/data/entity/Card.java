a25746dc73a48ccdb223f5b7056a3caea42531cd
seungyeonpark <siaayeon@gmail.com>
2022-05-13 00:18:29 +0900

    Feat: add builder

--- src/main/java/com/mandarin/mplus/data/entity/Card.java
+++ src/main/java/com/mandarin/mplus/data/entity/Card.java
@@ -1,6 +1,8 @@
 package com.mandarin.mplus.data.entity;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,6 +10,8 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
 public class Card {
 
     @Id
--- src/main/java/com/mandarin/mplus/data/entity/CardSet.java
+++ src/main/java/com/mandarin/mplus/data/entity/CardSet.java
@@ -1,6 +1,8 @@
 package com.mandarin.mplus.data.entity;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,6 +12,8 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
 public class CardSet {
 
     @Id
@@ -18,6 +22,8 @@ public class CardSet {
     private Long id;
 
     private String cardSetTitle;
+
+    private String cardSetCreator;
 
     private String cardSetCategories;
 
