c4ea2d1f14ff87431f9d84527d9a74252126671e
seungyeonpark <siaayeon@gmail.com>
2022-05-14 21:17:47 +0900

    Feat: add dto

--- src/main/java/com/mandarin/mplus/dto/CardDTO.java
+++ src/main/java/com/mandarin/mplus/dto/CardDTO.java
@@ -1,12 +1,29 @@
 package com.mandarin.mplus.dto;
 
+import com.mandarin.mplus.model.Card;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class CardDTO {
 
+    private Long id;
     private String cardTitle;
     private String cardContent;
+
+    public CardDTO(final Card card) {
+        this.cardTitle = card.getCardTitle();
+        this.cardContent = card.getCardContent();
+    }
+
+    public static Card toEntity(final CardDTO dto) {
+        return Card.builder()
+                .id(dto.getId())
+                .cardTitle(dto.getCardTitle())
+                .cardContent(dto.getCardContent())
+                .build();
+    }
 }
